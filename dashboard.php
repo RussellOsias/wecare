18749a0aeec009046aa32ffd75e9205f38629bd6
RussellOsias <159703036+RussellOsias@users.noreply.github.com>
2025-02-26 18:49:36 +0800

    database commit

--- dashboard.php
+++ dashboard.php
@@ -9,4 +9,3 @@ if (!isset($_SESSION['user_id'])) {
 echo "<h2>Welcome, " . htmlspecialchars($_SESSION['email']) . "!</h2>";
 echo "<p>This is your dashboard.</p>";
 echo "<a href='logout.php'>Logout</a>";
-?>
---
+++ wecare.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2025 at 11:49 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wecare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(200) NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `middle_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `phone_number` int(200) NOT NULL,
+  `address` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `email`, `password`, `phone_number`, `address`) VALUES
+(1, 'Russell', 'B', 'Osias', 'osiasrussell@gmail.com', '$2y$10$yLvu/qLhWk4wGqEPxIQAhuDk2HtaI3zVgW5FjkUTmdcPYJcWm.yZK', 2147483647, 'Baluan'),
+(2, 'Mark John', 'asd', 'asdadasd', 'djrussellosias@gmail.com', '$2y$10$gpL553RmuU5B9krgiqL14.qb6NiA8t2UMGROiQECtOg1QMqHDGSzm', 123, '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
