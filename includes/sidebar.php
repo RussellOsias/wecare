83f5bb05a47e4d52169a5e0573192c27013eeb32
Mark John <markjohnjopia1@gmail.com>
2025-02-26 20:27:52 +0800

    sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,16 @@
+<aside class="sidebar">
+      <h2>We Care</h2>
+      <ul>
+        <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
+        <li><a href="#"><i class="fas fa-users"></i> User Management</a></li>
+        <li><a href="#"><i class="fas fa-user"></i> Profile</a></li>
+        <li><a href="#"><i class="fas fa-envelope"></i> Messages</a></li>
+        <li><a href="#"><i class="fas fa-bell"></i> Notifications</a></li>
+        <li><a href="#"><i class="fa fa-comments"></i> Complaints</a></li>
+      </ul>
+
+      <button class="logout-btn" onclick="window.location.href='logout.php'">
+        <i class="fas fa-sign-out-alt"></i> Logout
+    </button>
+    
+</aside>
